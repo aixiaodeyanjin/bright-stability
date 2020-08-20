e5a221166b01b2fba1216a1c891e67cece2990ce
周荣所 <1758821765@qq.com>
2020-08-20 18:09:23 +0800

    更改楼栋标注为3D模型

--- src/assets/js/map/loader/BuildingLoader.js
+++ src/assets/js/map/loader/BuildingLoader.js
@@ -4,12 +4,13 @@ const INSTACNE = axios.create({})
 const URL = '/xlgc/data/mockerData/wending2.txt.json'
 const PARTY_ACTIVE_URL = '/xlgc/data/mockerData/wending.partyActive.json'
 const RESIDENT_ACTIVE_URL = '/xlgc/data/mockerData/wending.residentActive.json'
+const [MODEL_URL_BUILDING, MODEL_URL_COMMUNITY_CENTER, MODEL_URL_PARTY_CENTER] = ['/static/models/building.glb', '/static/models/community_center.glb', '/static/models/party_center.glb']
 export default function loadBuildingInfo (viewer) {
   INSTACNE.get(URL).then(resp => resp.data).then(({code, data}) => {
     data.forEach(element => {
       let x = element.local.split(',')[0] * 1
       let y = element.local.split(',')[1] * 1
-      let h = 5
+      let h = 60
       viewer.entities.add({
         id: element.id,
         name: element.name,
@@ -22,6 +23,10 @@ export default function loadBuildingInfo (viewer) {
         tenantLst: element.tenantLst,
         residentLst: element.residentLst,
         position: Cesium.Cartesian3.fromDegrees(x, y, h),
+        model: {
+          uri: MODEL_URL_BUILDING,
+          scale: 1
+        },
         label: {
           text: element.name,
           font: '12px Microsoft YaHei',
@@ -35,17 +40,6 @@ export default function loadBuildingInfo (viewer) {
           pixelOffset: new Cesium.Cartesian2(15, -2), // 偏移量
           disableDepthTestDistance: 1000000000
         },
-        billboard: {
-          image: require('@/assets/img/stability/mapicon/residents@2x.png'),
-          width: 20,
-          height: 20,
-          color: Cesium.Color.fromCssColorString(
-            '#ffffff'
-          ).withAlpha(1),
-          disableDepthTestDistance: 1000000000,
-          heightReference: Cesium.HeightReference.NONE,
-          verticalOrigin: Cesium.VerticalOrigin.BOTTOM
-        },
         attrInfo: '',
         description: ''
       })
@@ -54,7 +48,7 @@ export default function loadBuildingInfo (viewer) {
   INSTACNE.get(PARTY_ACTIVE_URL).then(resp => resp.data).then(element => {
     let x = element.local.split(',')[0] * 1
     let y = element.local.split(',')[1] * 1
-    let h = 0
+    let h = 38
     viewer.entities.add({
       id: element.id,
       code: element.id,
@@ -73,16 +67,9 @@ export default function loadBuildingInfo (viewer) {
         pixelOffset: new Cesium.Cartesian2(15, -2), // 偏移量
         disableDepthTestDistance: 1000000000
       },
-      billboard: {
-        image: require('@/assets/img/stability/mapicon/party_active2.png'),
-        width: 30,
-        height: 30,
-        color: Cesium.Color.fromCssColorString(
-          '#ffffff'
-        ).withAlpha(1),
-        disableDepthTestDistance: 1000000000,
-        heightReference: Cesium.HeightReference.NONE,
-        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+      model: {
+        uri: MODEL_URL_COMMUNITY_CENTER,
+        scale: 1
       },
       attrInfo: '',
       description: element.desc
@@ -91,7 +78,7 @@ export default function loadBuildingInfo (viewer) {
   INSTACNE.get(RESIDENT_ACTIVE_URL).then(resp => resp.data).then(element => {
     let x = element.local.split(',')[0] * 1
     let y = element.local.split(',')[1] * 1
-    let h = 0
+    let h = 38
     viewer.entities.add({
       id: element.id,
       code: element.id,
@@ -110,16 +97,9 @@ export default function loadBuildingInfo (viewer) {
         pixelOffset: new Cesium.Cartesian2(15, -2), // 偏移量
         disableDepthTestDistance: 1000000000
       },
-      billboard: {
-        image: require('@/assets/img/stability/mapicon/residentActive.png'),
-        width: 30,
-        height: 30,
-        color: Cesium.Color.fromCssColorString(
-          '#ffffff'
-        ).withAlpha(1),
-        disableDepthTestDistance: 1000000000,
-        heightReference: Cesium.HeightReference.NONE,
-        verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+      model: {
+        uri: MODEL_URL_PARTY_CENTER,
+        scale: 1
       },
       attrInfo: '',
       description: element.desc
