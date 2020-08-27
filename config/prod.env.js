a7926201917eefea8f7d0da0c1ef6996b1787b3e
周荣所 <1758821765@qq.com>
2020-08-27 17:11:39 +0800

    基础配置移到env.js中

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,36 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  MENUS: JSON.stringify([{
+    text: '首页',
+    url: 'http://bright.liaodukeji.com/home/index.html'
+  }, {
+    text: '市容',
+    url: 'http://bright.liaodukeji.com/#/1001'
+  }, {
+    text: '安全',
+    url: 'http://bright.liaodukeji.com/#/1002'
+  }, {
+    text: '稳定',
+    url: 'http://xlgc.liaodukeji.com/wending.html'
+  }, {
+    text: '党建',
+    url: 'http://bright.liaodukeji.com/home/partybuild.html'
+  }, {
+    text: '积分自治',
+    url: 'http://bright.liaodukeji.com/home/partyPoint.html'
+  }, {
+    text: '阵地建设',
+    url: 'http://bright.liaodukeji.com/#/1006'
+  }]),
+  baseURL: '"http://nbol.liaodukeji.com/api_bright/"',
+  ORG_ID: '"57fde62fceb011e9aa9100163e0d9e7f"',
+  APIS: JSON.stringify({
+    getAllData: 'bigDataScreem',
+    getActivityNum: 'activityNum',
+    getGridCounts: 'gridCounts',
+    getGridInfo: 'gridInfo',
+    getWeather: 'weather',
+    getLunarCalendar: 'lunarCalendar'
+  })
 }
--- src/assets/js/setup.js
+++ src/assets/js/setup.js
@@ -5,37 +5,18 @@ export default {
     Vue.use(Msg, {
       text: 'Hello world', duration: 3000, background: 'rgba(7,17,27,0.6)', position: 'center'
     })
-    const sysConfig = {
-      apiBase: 'http://nbol.liaodukeji.com/api_bright/',
-      // apiBase: "http://nb.liaodukeji.com/api_bright/",
-      orgId: '57fde62fceb011e9aa9100163e0d9e7f',
-      apiList: {
-        getAllData: 'bigDataScreem',
-        getActivityNum: 'activityNum',
-        getGridCounts: 'gridCounts',
-        getGridInfo: 'gridInfo',
-        getWeather: 'weather',
-        getLunarCalendar: 'lunarCalendar'
-      }
-    }
-
     const $api = axios.create({
-      baseURL: sysConfig.apiBase,
+      baseURL: process.env.baseURL,
       timeout: 20000
     })
-
     const $defaultApi = axios.create({})
-
     Vue.prototype.$getApi = (url, orgId) => {
-      return $api.get(url, {params: {'orgId': orgId || sysConfig.orgId}})
+      return $api.get(url, {params: {'orgId': orgId || process.env.ORG_ID}})
         .then(resp => resp.data)
     }
-
     Vue.prototype.$get = (url, params) => {
       return $defaultApi.get(url, {'params': { ...params }})
         .then(resp => resp.data)
     }
-
-    Vue.prototype.$appConfig = sysConfig
   }
 }
